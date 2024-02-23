4122669604478cf284ae1ae14fee252d74f13011
ValperDev <jdulcedev@gmail.com>
2024-02-22 21:31:41 -0500

    Add footer and navbar files and add test data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div></div>
+    <div>
+      
+    </div>
   )
 }
 
---
+++ src/components/layout/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom';
+
+export default function Navbar() {
+  return(
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/all-products'>All Products</NavLink>
+          </li>
+          <li>
+            <NavLink to='/men'>Men</NavLink>
+          </li>
+          <li>
+            <NavLink to='/women'>Women</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
