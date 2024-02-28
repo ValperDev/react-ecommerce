986dbc1d91a9002f2f85ebd5e92d2047961b2390
ValperDev <jdulcedev@gmail.com>
2024-02-27 21:32:03 -0500

    Add navbar styles

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -1,4 +1,5 @@
 import "./ReviewItem.css";
+import Star from "./icons/Star";
 
 export default function ReviewItem({rating, reviewContent, reviewDate, reviewTitle, isRecommended, reviewerName}) {
   return(
@@ -8,6 +9,9 @@ export default function ReviewItem({rating, reviewContent, reviewDate, reviewTit
         {isRecommended ? <span className="reviewer__recommend">I recommend this product</span> : ""}
       </div>
       <div className="review-content">
+        <div className="stars-container">
+          <Star fill={`--yellow`}/>
+        </div>
         <div className="review-top">
           <h2 className="review__title">{reviewTitle}</h2>
           <span className="review__name">{reviewDate}</span>
--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,16 +1,30 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+import Logo from "../../../assets/logo.png";
 import "./Navbar.css"
 
 export default function Navbar() {
   return(
     <header className="header">
-      <nav className="navbar">
-        <ul className="navbar__items">
-          <li className="navbar__item">
-            <NavLink className="navbar__link" to='/all-products'>All Products</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className="header-content">
+        <div className="logo">
+          <Link to="/" className="logo__link">
+            <img className='logo__image' src={Logo} alt="" />
+          </Link>
+        </div>
+        <nav className="navbar">
+          <ul className="navbar__items">
+            <li className="navbar__item">
+              <NavLink className="navbar__link" to='/all-products'>All Products</NavLink>
+            </li>
+            <li className="navbar__item">
+              <NavLink className="navbar__link" to='/women'>Men</NavLink>
+            </li>
+            <li className="navbar__item">
+              <NavLink className="navbar__link" to='/men'>Women</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
